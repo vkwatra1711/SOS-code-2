f27ac12bd08b0adf2e5fd961ec37de82a90e75f9
vkwatra1711 <52880809+vkwatra1711@users.noreply.github.com>
2019-07-24 19:11:56 +1000

    Add files via upload

---
+++ SOS.ino
@@ -0,0 +1,72 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+
+
+void loop() {
+  
+
+  // put your main code here, to run repeatedly:
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(500);
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(500);          
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(500);          
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(1500);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);          
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(1500);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(1500);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+
+ 
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(500);          
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(500);          
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);         
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+ delay(500);          
+ Serial.println(millis());
+ digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+ delay(1000);
+ Serial.println(millis());
+ 
+
+}
